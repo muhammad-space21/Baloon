afc345a1684a5fbee4d27de42e488d1524941da3
Daler Zokirov <zokirov.d@list.ru>
2020-02-20 12:26:29 +0500

    started creating an order-table component, and styling it

---
+++ src/components/order-table/order-table.jsx
@@ -0,0 +1,111 @@
+import React from 'react';
+
+import './order-table.styles.scss';
+
+import { Table, Form, Container } from 'react-bootstrap';
+
+import ButtonSelect from '../button-select/button-select';
+
+
+
+class OrderTable extends React.Component {
+    render() {
+        return (
+            <div className='order-table'>
+                <Container>
+                    <Table className='table' responsive >
+                        <thead className='head'>
+                            <tr className='tr'>
+                            <th className='th'>Brand</th>
+                            <th className='th'>Size</th>
+                            <th className='th'>Speed index</th>
+                            <th className='th'>Pattern</th>
+                            <th className='th'> 
+                                Max pressure 
+                                <br/>
+                                <span>(kpa/psi)</span>
+                            </th>
+                            <th className='th'>
+                                Quality
+                                <br/>
+                                certificate 
+                            </th>
+                            <th className='th'>
+                                Distance
+                                <br/>
+                                <span>(km)</span>
+                                </th>
+                            <th className='th'>
+                                Max Load
+                                <br/>
+                                weight
+                            </th>
+                            <th className='th'>
+                        
+                            </th>
+                            </tr>
+                        </thead>
+                        <tbody className='body'>
+                            <tr className='tr'>
+                            <td className='td'>
+                            <Form className='form-dropdown'> 
+                                    <Form.Group  controlId="formTableDropdown">
+                                        <Form.Control className='select' as="select">
+                                            <option></option>
+                                            <option>...</option>
+                                        </Form.Control>
+                                    </Form.Group>
+                                </Form>
+                            </td>
+                            <td className='td'>
+                                <Form className='form-dropdown'> 
+                                    <Form.Group  controlId="formTableDropdown">
+                                        <Form.Control className='select' as="select">
+                                            <option></option>
+                                            <option>...</option>
+                                        </Form.Control>
+                                    </Form.Group>
+                                </Form>
+                            </td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='button-holder'><ButtonSelect /></td>
+
+                            </tr>
+                            <tr className='tr'>
+                            <td></td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='button-holder'><ButtonSelect /></td>
+
+                            </tr>
+                            <tr className='tr'>
+                            <td></td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='td'>Table cell</td>
+                            <td className='button-holder'><ButtonSelect /></td>
+
+                            </tr>
+                        </tbody>
+                    </Table>
+                </Container>
+            </div>
+        );
+    };
+};
+
+export default OrderTable;
--- src/pages/order-page/order-page.jsx
+++ src/pages/order-page/order-page.jsx
@@ -3,28 +3,31 @@ import React from 'react';
 import './order-page.styles.scss';
 
 import NavbarMain from '../../components/navbar/navbar';
-import SelectButton from '../../components/button-select/button-select';
+import OrderTable from '../../components/order-table/order-table';
 import ButtonDelate from '../../components/button-delate/button-delate';
 import BackButton from '../../components/back-button/back-button';
 import ButtonGreen from '../../components/button-green/button-green';
 import Footer from '../../components/footer/footer';
 
+import { Container } from 'react-bootstrap';
 
 const OrderPage = () => (
     <div className='order-page'>
         <NavbarMain />
         <div className='order-page-body'>
-            <div className='title'>
-                <h1>Order Page</h1>
-            </div>
-            <SelectButton />
-            <ButtonDelate />
-            <div className='back-button-container'>
-                <BackButton />
-            </div>
-            <div className='green-button-container'>
-                <ButtonGreen />        
-            </div>
+            <Container>
+                <div className='title'>
+                    <h1>Order Page</h1>
+                </div>
+                <OrderTable />
+                <ButtonDelate />
+                <div className='buttons-container'>
+                    <div className='buttons'>
+                        <BackButton  className=' back-button-custom btn-back'/>
+                        <ButtonGreen className=' button-green-custom custom-btn btn-green'/>        
+                    </div>
+                </div>
+            </Container>
         </div>
         <Footer />
     </div>
