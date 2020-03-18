3ee6c6ec014676bbc00dffda22a9613d62bb28c3
Daler Zokirov <zokirov.d@list.ru>
2020-03-18 21:12:10 +0500

    imporoved  performance by using lazy and suspense components, and made ErrorBoundary component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
 
 import './App.css';
 
@@ -6,14 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import { ErrorBoundary } from './components/Error-boundary/error-boundary';
 
-import Homepage from './pages/homepage/homepage';
-import DescriptionPage from './pages/description-page/description-page';
-import SearchResultPage from './pages/search-result-page/search-result'; 
-import OrderPage from './pages/order-page/order-page';
-import ArticleCarPage from './pages/article-car-page/article-car-page';
-import ArticleTruckPage from './pages/article-truck-page/article-truck-page';
-import FormPage from './pages/form-page/form-page';
+const Homepage = lazy(() => import('./pages/homepage/homepage'));
+const DescriptionPage = lazy(() => import('./pages/description-page/description-page'));
+const SearchResultPage = lazy(() => import('./pages/search-result-page/search-result'));
+const OrderPage = lazy(() => import('./pages/order-page/order-page'));
+const ArticleCarPage = lazy(() => import('./pages/article-car-page/article-car-page'));
+const ArticleTruckPage = lazy(() => import('./pages/article-truck-page/article-truck-page'));
+const FormPage = lazy(() => import('./pages/form-page/form-page'));
 
 
 class App extends React.Component {
@@ -23,13 +24,17 @@ class App extends React.Component {
         <Provider store={store}>
           <div className='app'>
             <Switch>
-              <Route exact path='/' component={Homepage} />
-              <Route exact path='/description-page' component={DescriptionPage} />
-              <Route exact path='/search-result-page' component={SearchResultPage} />
-              <Route exact path='/order-page' component={OrderPage} />
-              <Route exact path='/article-car-page' component={ArticleCarPage} />
-              <Route exact path='/article-truck-page' component={ArticleTruckPage} />
-              <Route exact path='/form-page' component={FormPage} />
+              <ErrorBoundary>
+                <Suspense fallback={<div>Loading...</div>}>
+                  <Route exact path='/' component={Homepage} />
+                  <Route exact path='/description-page' component={DescriptionPage} />
+                  <Route exact path='/search-result-page' component={SearchResultPage} />
+                  <Route exact path='/order-page' component={OrderPage} />
+                  <Route exact path='/article-car-page' component={ArticleCarPage} />
+                  <Route exact path='/article-truck-page' component={ArticleTruckPage} />
+                  <Route exact path='/form-page' component={FormPage} />
+                </Suspense>
+              </ErrorBoundary>
             </Switch>
           </div>
         </Provider>
